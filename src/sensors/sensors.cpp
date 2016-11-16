20ebd6e082a139416a68b820146d0cab99519269
Fernando <coolh.bs@gmail.com>
2016-11-16 22:02:05 +0100

    Max y Min temp

--- src/sensors/sensors.cpp
+++ src/sensors/sensors.cpp
@@ -12,8 +12,8 @@ unsigned long last_sensor_store = 0;
 
 // Historico de sensores
 unsigned char store_index = 0;
-float air_temp[16];
-float hum[16];
+float air_temp[18];
+float hum[18];
 
 // Leo temperatura
 struct dhsensor dht22Read() {
@@ -32,6 +32,10 @@ struct dhsensor dht22Read() {
 void sensorStore() {
   // Variables
   int i;
+  float aTMax = -100;
+  float aTMin = 100;
+  float hMax = 0;
+  float hMin = 100;
 
   // Ejecuto cada STORE_DELAY milis del ultimo store
   if (millis() - last_sensor_store > STORE_DELAY) {
@@ -45,12 +49,28 @@ void sensorStore() {
       last_sensor_read = millis();
     }
     // Muevo valores viejos una posicion atras
-    for (i = 15; i > 0; i--) {
+    for (i = 1; i < 16; i++) {
       air_temp[i-1] = air_temp[i];
       hum[i-1] = hum[i];
     }
     // Escribo ultimos valores
     air_temp[15] = myDHT22.getTemperatureC();
     hum[15] = myDHT22.getHumidity();
+    last_sensor_store = millis();
+    // Busco MAX y MIN
+    for (i = 0; i < 16; i++) {
+      if (air_temp[i] > aTMax)
+        aTMax = air_temp[i];
+      if (air_temp[i] < aTMin)
+        aTMin = air_temp[i];
+      if (hum[i] > hMax)
+        hMax = air_temp[i];
+      if (air_temp[i] < hMin)
+        hMin = air_temp[i];
+    }
+    air_temp[16] = aTMin;
+    air_temp[17] = aTMax;
+    hum[16] = hMin;
+    hum[17] = hMax;
   }
 }
